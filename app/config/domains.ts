a1ac277b1d7995613a3c9af27290499faf77327d
czmatejt <tajovsky.matej@gmail.com>
2026-01-25 16:09:28 +0100

    fix domains.ts to use .env file

--- app/config/domains.ts
+++ app/config/domains.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://192.168.0.9:8000";
-export const WEBSITE_URL = "http://192.168.0.9:3000";
+export const API_URL = import.meta.env.VITE_API_URL || "http://192.168.0.9:8000";
+export const WEBSITE_URL = import.meta.env.VITE_WEBSITE_URL || "http://192.168.0.9:3000";
